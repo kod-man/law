a15d8148e42f88e1eac7262bd888cdeefc1052bb
sheikh sayam <sheikhsayam18@gmail.com>
2024-03-05 22:14:42 +0500

    Changes to Footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-
 const Footer = () => {
   return (
+    <>
     <div className="container">
       <div className="flex">
-        <div className="basis-full">Footer</div>
+        <div className="basis-full">Footer Content</div>
       </div>
     </div>
+    </>
   );
 };
 
