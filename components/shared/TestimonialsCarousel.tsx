b2dee46912ef741b27ed4767ee336765cde42bc9
Farhan <farhanshokat@gmail.com>
2024-04-01 14:51:44 +0500

    Testimonials Carousel Start

--- components/shared/TestimonialsCarousel.tsx
+++ components/shared/TestimonialsCarousel.tsx
@@ -10,13 +10,28 @@ export function TestimonialsCarousel() {
   return (
     <Carousel className="w-full">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        <CarouselItem>
+          <div className="p-1">
+            <span className="text-4xl font-semibold">1</span>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div className="p-1">
+            <span className="text-4xl font-semibold">1</span>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <div className="p-1">
+            <span className="text-4xl font-semibold">1</span>
+          </div>
+        </CarouselItem>
+        {/* {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
               <span className="text-4xl font-semibold">{index + 1}</span>
             </div>
           </CarouselItem>
-        ))}
+        ))} */}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
