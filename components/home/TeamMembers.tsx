11f87c9e01b9d95d1c7687096d5784a31c884760
sheikh sayam <sheikhsayam18@gmail.com>
2024-04-03 13:30:06 +0500

    home-team-member

--- components/home/TeamMembers.tsx
+++ components/home/TeamMembers.tsx
@@ -9,20 +9,9 @@ import testimonialbg from "@/public/assets/home-testimonial-bg.jpg";
 const TeamMembers = () => {
   return (
     <>
-    <div className="container pt-28 pb-24">
+    <div className="container pt-28 pb-52">
       <PrimaryHeadline text="Our Team" />
-    </div>
-    <div className="bg-grey relative overflow-hidden before:bg-[#1a2431] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:content-[''] before:opacity-70 before:z-[2]">
-      <Image
-          src={libraryicon}
-          alt="library-icon"
-          className="absolute top-0 z-[3] w-full rotate-180"
-        />
-      <Image src={testimonialbg} alt="testimonial-bg" fill={true} className="object-cover" />
-      <div className="container pt-24 pb-20 relative z-[3]">    
-        <h2 className="text-center pb-2 text-[40px] text-white font-bold relative z-[1] before:absolute before:left-0 before:right-0 before:m-auto before:bottom-0 before:w-36 before:h-[3px] before:bg-white mb-12">What Our Customers Say</h2>
-        <TeamMembersCarousel />
-      </div>
+      <TeamMembersCarousel />
     </div>
     </>
   );
--- components/shared/TeamMembersCarousel.tsx
+++ components/shared/TeamMembersCarousel.tsx
@@ -6,67 +6,144 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import testimonialimg1 from "@/public/assets/home-testimonial-img-1.jpeg";
-import testimonialimg2 from "@/public/assets/home-testimonial-img-2.jpeg";
-import testimonialimg3 from "@/public/assets/home-testimonial-img-3.jpeg";
+import teammember1 from "@/public/assets/team-member-1.jpg";
+import teammember2 from "@/public/assets/team-member-2.jpg";
+import teammember3 from "@/public/assets/team-member-3.jpg";
+import teammember4 from "@/public/assets/team-member-4.jpg";
+import teammember5 from "@/public/assets/team-member-5.jpg";
+import teammemberbefore from "@/public/assets/team-meber-before.svg";
 
 
 
 
 export function TeamMembersCarousel() {
   return (
-    <Carousel className="w-full">
-      <CarouselContent>
-        <CarouselItem className="">
-          <div className="*:text-white w-[850px] m-auto">
-            <Image
-            src={testimonialimg1}
-            alt="Testimonial Img"
-            className="w-24 h-24 rounded-full m-auto border-4 border-white"
-          />
-          <h3 className="text-[32px] text-center font-bold my-4">Opus Internatational Ltd</h3>
-          <p className="italic  text-left text-base mb-[18px]">When we needed the legal assistance Lawyero came to our rescue. They took our case and get it solved in no time. Now when ever we need any assistance for legal matters we look no further then the lawyers at Lawyero. Thanks team Lawyero you are the best!</p>
-          <h4 className="font-bold text-base text-center">CEO Operations</h4>
-          <h4 className="text-base text-center">Jessica Berrera</h4>
-          </div>
-        </CarouselItem>
-        <CarouselItem className="">
-          <div className="*:text-white w-[850px] m-auto">
-            <Image
-              src={testimonialimg2}
-              alt="Testimonial Img"
-              className="w-24 h-24 rounded-full m-auto border-4 border-white"
-            />
-            <h3 className="text-[32px] text-center font-bold my-4">Parag States</h3>
-            <p className="italic  text-left  mb-[18px] text-base">Our business was stuck in one of complex legal issue and one of our clients suggest team Lawyero. Now they are our official legal team which help us in numerous cases. Recommended to all!</p>
-            <h4 className="font-bold text-base text-center">Founder</h4>
-            <h4 className="text-base text-center">JohnKerry</h4>
-          </div>   
-        </CarouselItem>
-        <CarouselItem className="">
-          <div className="*:text-white w-[850px] m-auto">
-            <Image
-              src={testimonialimg3}
-              alt="Testimonial Img"
-              className="w-24 h-24 rounded-full m-auto border-4 border-white"
-            />
-            <h3 className="text-[32px] text-center font-bold my-4">Daily Times Network
-            </h3>
-            <p className="italic text-left mb-[18px] text-base">Team Lawyero is the best. Their team is awesome and when we visited their office their team welcomed us and make us feel really at home. They not only took our case but also helped us understand the complexity of the case. They fought for us and get the desired results soon. Thank you team lawyero! You are AWESOME.</p>
-            <h4 className="font-bold text-base text-center">Assistant Finance Manager</h4>
-            <h4 className="text-base text-center">Grant Josh</h4>
-          </div>
-        </CarouselItem>
-        {/* {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <span className="text-4xl font-semibold">{index + 1}</span>
+    <Carousel>
+      <CarouselContent className="pt-3">
+        <CarouselItem className="basis-1/4">
+            <div className="relative  before:absolute before:bg-black before:opacity-30 before::content-[''] before:h-full before:w-0 before:transition-all before:duration-300 before:ease-in hover:before:w-full before:left-0 before:top-0 cursor-pointer">              
+              <Image
+                src={teammember1}
+                alt="Team Member Img"
+                className="w-full"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute left-0 top-0"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute right-0 bottom-0 rotate-[180deg]"
+              />
             </div>
-          </CarouselItem>
-        ))} */}
+            <div className="*:text-primary-main *:italic *:text-center px-2 py-4 border-b-2 border-primary-main">
+              <h3 className="text-2xl mb-1">Robert Darren</h3>
+              <h4 className="text-xs">Bank & Finance lawyer</h4>
+            </div>
+        </CarouselItem>
+        <CarouselItem className="basis-1/4">
+          <div className="relative  before:absolute before:bg-black before:opacity-30 before::content-[''] before:h-full before:w-0 before:transition-all before:duration-300 before:ease-in hover:before:w-full before:left-0 before:top-0 cursor-pointer">              
+              <Image
+                src={teammember2}
+                alt="Team Member Img"
+                className="w-full"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute left-0 top-0"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute right-0 bottom-0 rotate-[180deg]"
+              />
+            </div>
+            <div className="*:text-primary-main *:italic *:text-center px-2 py-4 border-b-2 border-primary-main">
+              <h3 className="text-2xl mb-1">Tady Walsh</h3>
+              <h4 className="text-xs">Child Custody Lawyer</h4>
+            </div>
+        </CarouselItem>
+        <CarouselItem className="basis-1/4">
+            <div className="relative  before:absolute before:bg-black before:opacity-30 before::content-[''] before:h-full before:w-0 before:transition-all before:duration-300 before:ease-in hover:before:w-full before:left-0 before:top-0 cursor-pointer">              
+              <Image
+                src={teammember3}
+                alt="Team Member Img"
+                className="w-full"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute left-0 top-0"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute right-0 bottom-0 rotate-[180deg]"
+              />
+            </div>
+            <div className="*:text-primary-main *:italic *:text-center px-2 py-4 border-b-2 border-primary-main">
+              <h3 className="text-2xl mb-1">Alanna</h3>
+              <h4 className="text-xs">Business Lawyer</h4>
+            </div>
+        </CarouselItem>
+        <CarouselItem className="basis-1/4">
+            <div className="relative  before:absolute before:bg-black before:opacity-30 before::content-[''] before:h-full before:w-0 before:transition-all before:duration-300 before:ease-in hover:before:w-full before:left-0 before:top-0 cursor-pointer">              
+              <Image
+                src={teammember4}
+                alt="Team Member Img"
+                className="w-full"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute left-0 top-0"
+              />
+              <Image
+                src={teammemberbefore}
+                alt="Team Member Before"
+                className="w-11 h-11 absolute right-0 bottom-0 rotate-[180deg]"
+              />
+            </div>
+            <div className="*:text-primary-main *:italic *:text-center px-2 py-4 border-b-2 border-primary-main">
+              <h3 className="text-2xl mb-1">Frank Moses</h3>
+              <h4 className="text-xs">Injury Expert Attorney</h4>
+            </div>
+        </CarouselItem>
+        <CarouselItem className="basis-1/4">
+            <div className="relative  before:absolute before:bg-black before:opacity-30 before::content-[''] before:h-full before:w-0 before:transition-all before:duration-300 before:ease-in hover:before:w-full before:left-0 before:top-0 cursor-pointer">              
+                <Image
+                  src={teammember5}
+                  alt="Team Member Img"
+                  className="w-full"
+                />
+                <Image
+                  src={teammemberbefore}
+                  alt="Team Member Before"
+                  className="w-11 h-11 absolute left-0 top-0"
+                />
+                <Image
+                  src={teammemberbefore}
+                  alt="Team Member Before"
+                  className="w-11 h-11 absolute right-0 bottom-0 rotate-[180deg]"
+                />
+            </div>
+            <div className="*:text-primary-main *:italic *:text-center px-2 py-4 border-b-2 border-primary-main">
+              <h3 className="text-2xl mb-1">John Doe</h3>
+              <h4 className="text-xs">International Law Lawyer</h4>
+            </div>
+        </CarouselItem>
       </CarouselContent>
-      <CarouselPrevious className="left-0 border-2 border-white opacity-100 disabled:opacity-100 rounded-none h-12 w-12 text-white bg-transparent" />
-      <CarouselNext className="right-0 border-2 border-white opacity-100 disabled:opacity-100 rounded-none h-12 w-12 text-white bg-transparent" />
+        <div className="w-full flex justify-center">
+          <div className="team-member-arrow absolute w-28 h-12 pt-14 mt-14">
+            <CarouselPrevious className="left-0 border-2 border-black opacity-100 disabled:opacity-100 rounded-none h-12 w-12 text-black  hover:bg-white bg-white"  />
+            <CarouselNext className="right-0 border-2 border-black opacity-100 disabled:opacity-100 rounded-none h-12 w-12 text-black  hover:bg-white bg-white" />
+          </div>
+        </div>
     </Carousel>
+    
+    
   );
 }
