3b1f6c6e5a9ea43f64ed8f045907c9cbebf321c7
Farhan <farhanshokat@gmail.com>
2024-04-26 22:08:13 +0500

    FAQs Props Path Fix 2

--- components/shared/Accordions.tsx
+++ components/shared/Accordions.tsx
@@ -18,7 +18,7 @@ const Accordions = ({ data }: { data: FAQsProps[] }) => {
           <AccordionTrigger className="rounded-md border-2 border-background px-4 py-2 text-left text-base font-semibold text-background hover:no-underline focus:bg-background focus:text-white">
             {item.question}
           </AccordionTrigger>
-          <AccordionContent className="relative mt-5 rounded-md border-2 border-background p-4 text-base leading-6 before:absolute before:-top-[22px] before:left-[30px] before:h-5 before:w-[2px] before:bg-background before:content-[''] after:absolute after:-top-[22px] after:right-[30px] after:h-5 after:w-[2px] after:bg-background after:content-['']">
+          <AccordionContent className="relative mt-5 rounded-md border-2 border-background p-4 text-base leading-6 before:absolute before:left-[30px] before:top-[-22px] before:h-5 before:w-[2px] before:bg-background before:content-[''] after:absolute after:right-[30px] after:top-[-22px] after:h-5 after:w-[2px] after:bg-background after:content-['']">
             {item.answer}
           </AccordionContent>
         </AccordionItem>
