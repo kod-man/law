3aad639c0dd0eacd0d05712de9fe6616947d8488
Farhan <farhanshokat@gmail.com>
2024-04-26 22:10:38 +0500

    Inner Banner Fix

--- components/global/inner-banner.tsx
+++ components/global/inner-banner.tsx
@@ -9,15 +9,17 @@ const InnerBanner = ({
   image?: StaticImageData;
 }) => {
   return (
-    <div className="py-[70px] relative overflow-hidden before:bg-background before:absolute before:top-0 before:left-0 before:w-full before:h-full before:content-[''] before:opacity-70 before:z-[2]">
+    <div className="relative overflow-hidden py-[70px] before:absolute before:left-0 before:top-0 before:z-[2] before:size-full before:bg-background before:opacity-70 before:content-['']">
       <Image
-        src={image ? image : innerbanner}
+        src={image || innerbanner}
         alt="Page Banner With Title"
         fill={true}
         className="object-cover"
       />
       <div className="container relative z-[3]">
-        <h1 className="lg:text-[40px] text-[30px] text-white font-bold text-center">{text}</h1>
+        <h1 className="text-center text-[30px] font-bold text-white lg:text-[40px]">
+          {text}
+        </h1>
       </div>
     </div>
   );
