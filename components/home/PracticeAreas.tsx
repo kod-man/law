99ce3b1a9dd994a30dd971b3f6bb1a86504495b0
ufukuysal <ufuk.uysal@hybridcore.eu>
2025-05-17 09:39:46 +0200

    practise area with slider

--- components/home/PracticeAreas.tsx
+++ components/home/PracticeAreas.tsx
@@ -1,22 +1,61 @@
-import PrimaryHeadline from "../global/primary-headline";
+"use client";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/free-mode";
+import { Autoplay, FreeMode } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import { HomepagePAs } from "@/lib/dataWithJSX";
+import PrimaryHeadline from "../global/primary-headline";
 import IconCardWithLink from "../shared/IconCardWithLink";
 
 const PracticeAreas = () => {
   return (
-    <div className="container smpt-20 pt-16 lg:pb-32 sm:pb-20 pb-16 relative">
+    <div className="container pt-16 pb-16 lg:pb-32 sm:pt-20 sm:pb-20 relative">
       <PrimaryHeadline text="Faaliyet Alanlarımız" />
-      <div className="flex flex-wrap -mx-2">
+      <Swiper
+        modules={[FreeMode, Autoplay]}
+        loop={true} // 🔁 Baştan sona döngü
+        spaceBetween={16}
+        freeMode={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+          reverseDirection: false, // ⬅️ Bu false olduğunda sağa doğru akar
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1.2,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+          1280: {
+            slidesPerView: 5,
+          },
+        }}
+        className="mt-8"
+      >
         {HomepagePAs.map((item, index) => (
-          <IconCardWithLink
-            key={`PA${item.id}`}
-            link={item.link}
-            pa={item.PA}
-            iconName={item.iconName}
-            cardBgClass={index % 2 === 0 ? "bg-background" : "bg-primary-main"}
-          />
+          <SwiperSlide key={`PA${item.id}`} className="!h-[260px]">
+            <IconCardWithLink
+              link={item.link}
+              pa={item.PA}
+              iconName={item.iconName}
+              cardBgClass={
+                index % 2 === 0 ? "bg-background" : "bg-primary-main"
+              }
+            />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
--- components/shared/IconCardWithLink.tsx
+++ components/shared/IconCardWithLink.tsx
@@ -15,16 +15,16 @@ const IconCardWithLink = ({
   cardBgClass,
 }: IconCardWithLinkProps) => {
   return (
-    <div className="group mb-4 flex min-h-64 basis-full px-2 text-center md:basis-1/2 lg:basis-1/4">
-      <div className={`${cardBgClass}  w-full`}>
-        <Link
-          className="transition-box-shadow flex size-full flex-col items-center justify-center duration-500 ease-in-out group-hover:shadow-[0_6px_13px_rgba(26,36,49,0.35)]"
-          href={link}
-        >
-          <span className="inline-block">{iconName}</span>
-          <h3 className="mt-5 text-2xl font-semibold text-white">{pa}</h3>
-        </Link>
-      </div>
+    <div className={`${cardBgClass} h-full w-full rounded-lg`}>
+      <Link
+        className="transition-box-shadow flex h-full w-full flex-col items-center justify-center p-6 duration-500 ease-in-out hover:shadow-[0_6px_13px_rgba(26,36,49,0.35)]"
+        href={link}
+      >
+        <span className="inline-block">{iconName}</span>
+        <h3 className="mt-5 text-2xl font-semibold text-white text-center">
+          {pa}
+        </h3>
+      </Link>
     </div>
   );
 };
