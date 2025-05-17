e42b11dda58a0e877ca7d797718a07f319a63644
ufukuysal <ufuk.uysal@hybridcore.eu>
2025-05-17 09:19:05 +0200

    FAQS animation

--- components/shared/FAQItem.tsx
+++ components/shared/FAQItem.tsx
@@ -1,5 +1,8 @@
-import { CircleHelp } from "lucide-react";
-import React from "react";
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { ChevronDown, ChevronUp, CircleHelp } from "lucide-react";
+import { useState } from "react";
 
 interface FAQProp {
   question: string;
@@ -7,18 +10,40 @@ interface FAQProp {
 }
 
 const FAQItem = ({ question, answer }: FAQProp) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="mb-12 pr-0 sm:basis-full lg:basis-1/2 lg:pr-6">
-      <div className="flex">
+    <div className="mb-6 pr-0 sm:basis-full lg:basis-1/2 lg:pr-6">
+      <div
+        className="flex cursor-pointer items-start"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <CircleHelp
           size="30"
           className="mt-1 shrink-0 rounded-full bg-white text-primary-main"
         />
-        <div className="pl-4">
-          <h3 className="mb-3 text-2xl font-bold text-background">
-            {question}
-          </h3>
-          <p className="text-base text-[#333]">{answer}</p>
+        <div className="pl-4 flex-1">
+          <div className="flex justify-between items-center">
+            <h3 className="mb-3 text-2xl font-bold text-background">
+              {question}
+            </h3>
+            {isOpen ? <ChevronUp size={24} /> : <ChevronDown size={24} />}
+          </div>
+
+          <AnimatePresence initial={false}>
+            {isOpen && (
+              <motion.div
+                key="answer"
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                transition={{ duration: 0.3 }}
+                className="overflow-hidden"
+              >
+                <p className="text-base text-[#333]">{answer}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </div>
