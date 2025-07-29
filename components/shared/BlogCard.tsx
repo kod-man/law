9d73fbebee1407035072dde99e1db59f4d777603
Ahmet Uluisik <ahmet55uluisik05@gmail.com>
2025-07-29 23:38:47 +0300

    aa

--- components/shared/BlogCard.tsx
+++ components/shared/BlogCard.tsx
@@ -11,26 +11,38 @@ interface BlogCardProps {
 
 const BlogCard = ({ imageURL, date, linkHref, title }: BlogCardProps) => {
   return (
-    <div className="group basis-1/3">
+    <div className="group flex flex-col h-full bg-white shadow-lg rounded-none">
       <div className="overflow-hidden">
         <Image
           src={imageURL}
-          alt="Home BLog Post"
-          className="scale-110 transition-transform duration-500 ease-in-out group-hover:scale-100"
+          alt="Home Blog Post"
+          className="w-full h-60 object-cover scale-110 transition-transform duration-500 ease-in-out group-hover:scale-100"
         />
       </div>
-      <div className="bg-white p-5">
-        <span className="mb-4 inline-block text-sm text-primary-main">
-          {formatDate(date)}
-        </span>
-        <h3>
+
+      <div className="flex flex-col justify-between flex-grow p-6">
+        <div>
+          <span className="mb-2 inline-block text-sm text-primary-main">
+            {formatDate(date)}
+          </span>
+          <h3 className="mb-4">
+            <Link
+              href={linkHref}
+              className="inline-block cursor-pointer text-lg font-semibold text-background hover:text-primary-main"
+            >
+              {title}
+            </Link>
+          </h3>
+        </div>
+
+        <div className="flex justify-end mt-auto">
           <Link
             href={linkHref}
-            className="inline-block cursor-pointer text-base font-semibold text-background hover:text-primary-main"
+            className="text-xs font-medium text-white bg-primary-main hover:bg-primary-dark transition px-3 py-1"
           >
-            {title}
+            Devamını Oku
           </Link>
-        </h3>
+        </div>
       </div>
     </div>
   );
